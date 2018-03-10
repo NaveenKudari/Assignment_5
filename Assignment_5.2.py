1a4221f6e173a8bd11e5b035e8ca699814d15ce1
NaveenKudari <35500577+NaveenKudari@users.noreply.github.com>
2018-03-10 13:14:13 +0530

    Add files via upload

---
+++ Assignment_5.2.py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# In[4]:
+
+
+subjects=["Americans ", " Indians"]
+verbs=[" play", " watch"]
+objects=[" Baseball", " Cricket"]
+
+for sub in subjects:
+    for ver in verbs:
+        print(sub+ver+objects[0])
+        print(sub+ver+objects[1])
+
